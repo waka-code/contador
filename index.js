a797c90c8742732019f57ebb45cac6151e0cfc95
Waddimi Saint Louis <94648767+waka-code@users.noreply.github.com>
2022-05-08 03:05:49 +0800

    contador

---
+++ index.js
@@ -0,0 +1,38 @@
+window.onload = inicio;
+
+const cero = num =>{
+    if(num.toString().length < 2)
+    return `0`.concat(num);
+    return num;
+}
+
+
+function inicio(){
+    let reloj = document.getElementById(`reloj`)
+    let day = document.getElementById(`day`)
+    let dias =[`Lunes`,`Martes`,`Miercoles`,`Jueves`,`Viernes`,`Sabado`,`Domingo`]
+    let meses =[
+            `Enero`,`Febrero`,`Marzo`,`Abril`,
+            `Mayo`, `Junio`, `Julio`, `Agosto`,
+            `Septiembre`, `Octubre`,`Noviembre`,`Diciembre`
+       ]
+
+    let today = new Date()
+    let hr = cero(today.getHours());
+    let min = cero(today.getMinutes());
+    let sec = cero(today.getSeconds());
+    let dia = today.getDay();
+    let mes = today.getMonth();
+    let ano = (today.getYear()+1900);
+
+    let m=meses[mes];
+    let d=dias[dia];
+    reloj.innerHTML = `${hr}:${min}:${sec}`
+    day.innerHTML = `${d}, ${m} Del ${ano} `
+
+}
+
+inicio()
+setInterval(inicio, 1000)
+
+
